7ea6c33bb38d134a5463b7a6e1da86df272d95b1
DarcoProgramador <denangel123@gmail.com>
2023-05-29 17:37:47 -0600

    add provides for GoogleSinIn in AuthModule

--- app/src/main/java/com/proyecpg/hartarte/di/AuthModule.kt
+++ app/src/main/java/com/proyecpg/hartarte/di/AuthModule.kt
@@ -1,12 +1,25 @@
 package com.proyecpg.hartarte.di
 
+import android.app.Application
+import android.content.Context
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.proyecpg.hartarte.R
 import com.proyecpg.hartarte.data.auth.AuthRepository
 import com.proyecpg.hartarte.data.auth.AuthRepositoryImp
+import com.proyecpg.hartarte.utils.Constants.SIGN_IN_REQUEST
+import com.proyecpg.hartarte.utils.Constants.SIGN_UP_REQUEST
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -18,5 +31,52 @@ class AuthModule {
     @Singleton
     @Provides
     fun providesAuthRepository(impl: AuthRepositoryImp): AuthRepository = impl
+
+    @Provides
+    fun provideOneTapClient(
+        @ApplicationContext
+        context: Context
+    ) : SignInClient = Identity.getSignInClient(context)
+
+    @Provides
+    @Named(SIGN_IN_REQUEST)
+    fun provideSignInRequest(
+        app: Application
+    ) : BeginSignInRequest = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(app.getString(R.string.web_client_id))
+                .setFilterByAuthorizedAccounts(true)
+                .build())
+        .setAutoSelectEnabled(true)
+        .build()
+
+    @Provides
+    @Named(SIGN_UP_REQUEST)
+    fun provideSignUpRequest(
+        app: Application
+    ) : BeginSignInRequest = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(app.getString(R.string.web_client_id))
+                .setFilterByAuthorizedAccounts(false)
+                .build())
+        .build()
+
+    @Provides
+    fun provideGoogleSignInOptions(
+        app: Application
+    ) : GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(app.getString(R.string.web_client_id))
+        .requestEmail()
+        .build()
+
+    @Provides
+    fun provideGoogleSignInClient(
+        app: Application,
+        options: GoogleSignInOptions
+    ) : GoogleSignInClient = GoogleSignIn.getClient(app, options)
 
 }
