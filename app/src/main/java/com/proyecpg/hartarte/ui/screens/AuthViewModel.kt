06934ce75a35241266dc3fde471082779d2ac8aa
DarcoProgramador <denangel123@gmail.com>
2023-05-30 11:48:15 -0600

    Add login sates in AuthViewModel

--- app/src/main/java/com/proyecpg/hartarte/ui/screens/AuthViewModel.kt
+++ app/src/main/java/com/proyecpg/hartarte/ui/screens/AuthViewModel.kt
@@ -1,11 +1,64 @@
 package com.proyecpg.hartarte.ui.screens
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseUser
 import com.proyecpg.hartarte.data.auth.AuthRepository
+import com.proyecpg.hartarte.ui.screens.login.LoginState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class AuthViewModel @Inject constructor(
     private val authRepository:AuthRepository)
     : ViewModel() {
-        
+    private val _state = MutableStateFlow(LoginState())
+    val state = _state.asStateFlow()
+
+    private val _logged = MutableStateFlow(false)
+    val logged = _logged.asStateFlow()
+
+    private val currentUser: FirebaseUser?
+        get() = authRepository.currentUser
+
+    init {
+        viewModelScope.launch {
+            isLogged()
+        }
+    }
+
+    suspend fun isLogged(){
+        if (currentUser != null){
+            _logged.emit(true)
+        }else{
+            _logged.emit(false)
+        }
+    }
+
+    fun resetState() {
+        _state.update { LoginState() }
+    }
+
+    fun login(){
+        TODO("Hacer la funcion para loguear")
+    }
+
+    fun register(){
+        TODO("Hacer la funcion para registrar")
+    }
+
+
+    fun signInWithCredentials(credential: AuthCredential){
+        TODO("Hacer logueo con credenciales")
+    }
+
+    fun oneTapSignIn(){
+        TODO("Hacer llamado al oneTap de google")
+    }
 }
