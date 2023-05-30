1932fb1c662fdd1d71408ddcbc0a3bbfff1934b5
DarcoProgramador <denangel123@gmail.com>
2023-05-30 10:05:10 -0600

    Add HomeScreen Schema

---
+++ app/src/main/java/com/proyecpg/hartarte/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,45 @@
+package com.proyecpg.hartarte.ui.screens.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.proyecpg.hartarte.R
+
+@Composable
+fun HomeScreen(
+    //OnLoginClick
+    //OnRegisterClick
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier.size(150.dp),
+            painter = painterResource(id = R.drawable.img_logo),
+            contentDescription = stringResource(R.string.app_name)
+        )
+
+        Spacer(modifier = Modifier.size(50.dp))
+
+        Text(text = "HomeScreen")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoginScreen(){
+    HomeScreen()
+}
