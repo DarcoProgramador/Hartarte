2e2de644667f86432dd8acea04726887484b6012
DarcoProgramador <denangel123@gmail.com>
2023-06-03 14:19:20 -0600

    add need implementations test
    add implementation for test, mocck, corutines test and organize the graddle app

--- app/src/test/java/com/proyecpg/hartarte/ui/screens/register/RegisterViewModelTest.kt
+++ app/src/test/java/com/proyecpg/hartarte/ui/screens/register/RegisterViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.proyecpg.hartarte.ui.screens.register
 
 import com.proyecpg.hartarte.data.auth.AuthRepository
+import com.proyecpg.hartarte.data.auth.AuthRepositoryImp
+import io.mockk.mockk
 import org.junit.Assert.*
 
 class RegisterViewModelTest{
 
-    private val repository: AuthRepository = mocck()
-    private val viewModel = RegisterViewModel()
+    private val repository: AuthRepository = mockk()
+    private val viewModel = RegisterViewModel(repository)
 }
