fec93b1d81ae4a79e2b5c4f491dcfaab9db622cd
DarcoProgramador <denangel123@gmail.com>
2023-06-03 14:02:58 -0600

    add mockito
    add mockito implementationTest to graddle of app module

--- app/src/test/java/com/proyecpg/hartarte/ui/screens/register/RegisterViewModelTest.kt
+++ app/src/test/java/com/proyecpg/hartarte/ui/screens/register/RegisterViewModelTest.kt
@@ -1,7 +1,10 @@
 package com.proyecpg.hartarte.ui.screens.register
 
+import com.proyecpg.hartarte.data.auth.AuthRepository
 import org.junit.Assert.*
 
 class RegisterViewModelTest{
-    
+
+    private val repository: AuthRepository = mocck()
+    private val viewModel = RegisterViewModel()
 }
