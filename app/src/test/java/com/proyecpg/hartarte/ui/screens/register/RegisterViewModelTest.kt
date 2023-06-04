be6c579fc34eb1e0b56d320931af99c39e566a2c
DarcoProgramador <denangel123@gmail.com>
2023-06-04 17:31:37 -0600

    Add second test
    Add second test to RegisterViewModelTest named "Test cuando el correo es incorrecto"

--- app/src/test/java/com/proyecpg/hartarte/ui/screens/register/RegisterViewModelTest.kt
+++ app/src/test/java/com/proyecpg/hartarte/ui/screens/register/RegisterViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.proyecpg.hartarte.ui.screens.register
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.proyecpg.hartarte.R
 import com.proyecpg.hartarte.data.auth.AuthRepository
 import com.proyecpg.hartarte.utils.Resource
 import kotlinx.coroutines.Dispatchers
@@ -24,7 +25,7 @@ import org.mockito.kotlin.any
 class RegisterViewModelTest{
 
     @Mock
-    private lateinit var repository : AuthRepository
+    private lateinit var repository : AuthRepository //Anotacion de mockito
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
@@ -34,8 +35,7 @@ class RegisterViewModelTest{
 
     @Before
     fun setUp() {
-        //viewModel = RegisterViewModel(repository)
-        MockitoAnnotations.openMocks(this)
+        MockitoAnnotations.openMocks(this) //Cargango anotaciones Mockito
         Dispatchers.setMain(dispatcher)
     }
 
@@ -47,7 +47,7 @@ class RegisterViewModelTest{
     @Test
     fun `Test cuando los datos pasados estan correctos`() = runTest{
 
-        Mockito.`when`(repository.signup(any<String>(), any<String>(), any<String>())).thenReturn(Resource.Success(null))
+        Mockito.`when`(repository.signup(any(), any(), any())).thenReturn(Resource.Success(null))
 
         val sut = RegisterViewModel(repository)
 
@@ -55,5 +55,18 @@ class RegisterViewModelTest{
 
         dispatcher.scheduler.advanceUntilIdle()
         assertEquals(RegisterState(isRegisterSuccessful = true, isLoading = false), sut.stateRegister.value)
+    }
+
+    @Test
+    fun `Test cuando el correo es incorrecto`() = runTest{
+
+        Mockito.`when`(repository.signup(any(), any(), any())).thenReturn(Resource.Success(null))
+
+        val sut = RegisterViewModel(repository)
+
+        sut.process(RegisterEvent.RegisterClicked("Holabb","hola","Holabb*123","Holabb*123"))
+
+        dispatcher.scheduler.advanceUntilIdle()
+        assertEquals(RegisterState(isLoading = false, registerError = R.string.error_invalid_email.toString()), sut.stateRegister.value)
     }
 }
