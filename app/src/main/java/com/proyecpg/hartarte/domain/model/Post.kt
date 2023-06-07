5bed7474a93205cf91d67abb7340d0f6b11d2d40
DarcoProgramador <denangel123@gmail.com>
2023-06-07 16:37:59 -0600

    update names on Post modle

--- app/src/main/java/com/proyecpg/hartarte/domain/model/Post.kt
+++ app/src/main/java/com/proyecpg/hartarte/domain/model/Post.kt
@@ -1,11 +1,11 @@
 package com.proyecpg.hartarte.domain.model
 
 import com.google.firebase.Timestamp
-import com.google.firebase.firestore.local.ReferenceSet
-
+import com.google.firebase.firestore.DocumentId
 data class Post (
+    @DocumentId val id: String,
     val titulo: String = "",
-    val descipcion: String = "",
+    val descripcion: String = "",
     val imagen: String? = null,
     val userRef: String? = "",
     val creation_time: Timestamp? = null
