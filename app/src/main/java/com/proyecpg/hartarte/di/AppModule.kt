6fbc97a7f6f43818dbe4a14d5b1f4b26b6d0acee
DarcoProgramador <denangel123@gmail.com>
2023-06-07 12:47:04 -0600

    Add firebase
    add firebase in graddle and add AppModule for FireStore

---
+++ app/src/main/java/com/proyecpg/hartarte/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.proyecpg.hartarte.di
+
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.Query.Direction.DESCENDING
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.proyecpg.hartarte.utils.Constants.PAGE_SIZE
+import com.proyecpg.hartarte.utils.Constants.POST_COLLECTION
+import com.proyecpg.hartarte.utils.Constants.TIME_STAMP
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+    @Provides
+    fun providequeryPostByCreationTime(): Query = Firebase.firestore
+        .collection(POST_COLLECTION)
+        .orderBy(TIME_STAMP, DESCENDING)
+        .limit(PAGE_SIZE)
+
+    @Provides
+    fun provideProductsPagingSource(
+        queryPostByCreationTime: Query
+    ) = ProductsPagingSource(
+        queryPostByCreationTime = queryProductsByName
+    )
+
+    @Provides
+    fun providePagingConfig() = PagingConfig(
+        pageSize = PAGE_SIZE.toInt()
+    )
+
+    @Provides
+    fun provideProductsRepository(
+        source: ProductsPagingSource,
+        config: PagingConfig
+    ): ProductsRepository = ProductsRepositoryImpl(
+        source = source,
+        config = config
+    )
+}
--- app/src/main/java/com/proyecpg/hartarte/utils/Constants.kt
+++ app/src/main/java/com/proyecpg/hartarte/utils/Constants.kt
@@ -16,4 +16,12 @@ object Constants {
     //Names
     const val SIGN_IN_REQUEST = "signInRequest"
     const val SIGN_UP_REQUEST = "signUpRequest"
+
+    //FireBaseUser
+    const val USERS_COLLECTION = "users"
+
+    //FireBasePost
+    const val POST_COLLECTION = "posts"
+    const val TIME_STAMP = "creation_time"
+    const val PAGE_SIZE = 10L
 }
