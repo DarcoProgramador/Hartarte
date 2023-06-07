7a303b0eb7284c067a8a3a4419c3ac20fd0687a2
DarcoProgramador <denangel123@gmail.com>
2023-06-07 16:20:32 -0600

    update PostPagingSource
    update PostPagingSource and Post to make it compatible

--- app/src/main/java/com/proyecpg/hartarte/domain/model/Post.kt
+++ app/src/main/java/com/proyecpg/hartarte/domain/model/Post.kt
@@ -1,19 +1,17 @@
 package com.proyecpg.hartarte.domain.model
 
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.local.ReferenceSet
+
 data class Post (
-    val titulo: String,
-    val descipcion: String,
+    val titulo: String = "",
+    val descipcion: String = "",
     val imagen: String? = null,
-    val userRef: String,
-    val comentarios : Comentarios,
-    val likes: Likes
+    val userRef: String? = "",
+    val creation_time: Timestamp? = null
 )
 
 data class Comentarios(
     val comentario: String,
-    val userRef: String
-)
-
-data class Likes(
     val userRef: String
 )
