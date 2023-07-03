8b16e2ee004c1eeabd49ad52c460e9b2595080f2
DarcoProgramador <denangel123@gmail.com>
2023-07-03 11:32:58 -0600

    update CreatePostScreenViewModel
    add MutableStateFlow of his State and funtion resetState

--- app/src/main/java/com/proyecpg/hartarte/ui/screens/post/create/CreatePostScreenViewModel.kt
+++ app/src/main/java/com/proyecpg/hartarte/ui/screens/post/create/CreatePostScreenViewModel.kt
@@ -2,12 +2,21 @@ package com.proyecpg.hartarte.ui.screens.post.create
 
 import androidx.lifecycle.ViewModel
 import com.proyecpg.hartarte.data.post.PostRepository
+import com.proyecpg.hartarte.ui.screens.register.RegisterState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class CreatePostScreenViewModel @Inject constructor(
     private val postRepository: PostRepository
 ) : ViewModel() {
+    private val _stateCreatePost = MutableStateFlow(CreatePostState())
+    val stateCreatePost = _stateCreatePost.asStateFlow()
 
+    fun resetState() {
+        _stateCreatePost.update { CreatePostState() }
+    }
 }
