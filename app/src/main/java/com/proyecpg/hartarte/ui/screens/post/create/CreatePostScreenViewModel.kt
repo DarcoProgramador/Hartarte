3fdadb4bbd104a70c07eb17c11b8cb437b5b039e
DarcoProgramador <denangel123@gmail.com>
2023-07-03 11:48:27 -0600

    update addPost
    update addPost in CreatePostScreenViewModel for return error whenn the fields are emptys

--- app/src/main/java/com/proyecpg/hartarte/ui/screens/post/create/CreatePostScreenViewModel.kt
+++ app/src/main/java/com/proyecpg/hartarte/ui/screens/post/create/CreatePostScreenViewModel.kt
@@ -1,12 +1,15 @@
 package com.proyecpg.hartarte.ui.screens.post.create
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.proyecpg.hartarte.R
 import com.proyecpg.hartarte.data.post.PostRepository
-import com.proyecpg.hartarte.ui.screens.register.RegisterState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,5 +21,40 @@ class CreatePostScreenViewModel @Inject constructor(
 
     fun resetState() {
         _stateCreatePost.update { CreatePostState() }
+    }
+
+    fun addPost(images : List<Uri>,
+                titulo: String,
+                descripcion: String
+    ){
+        viewModelScope.launch {
+            _stateCreatePost.update { it.copy(isLoading = true)}
+
+            if (images.isEmpty()){
+                setError(R.string.error_missing_images.toString())
+                return@launch
+            }
+
+            if (titulo.isBlank()){
+                setError(R.string.error_missing_title.toString())
+                return@launch
+            }
+
+            if (descripcion.isBlank()){
+                setError(R.string.error_missing_description.toString())
+                return@launch
+            }
+
+
+        }
+    }
+
+    private fun setError(error: String){
+        _stateCreatePost.update {
+            it.copy(
+                isLoading = false,
+                createPostError = error
+            )
+        }
     }
 }
