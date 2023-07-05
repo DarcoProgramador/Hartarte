6a2bba38dfebd5b65b2a32d8556a533207158fcc
DarcoProgramador <denangel123@gmail.com>
2023-07-04 23:13:26 -0600

    add SwipeRefresh on SearchScreen

--- app/src/main/java/com/proyecpg/hartarte/MainActivity.kt
+++ app/src/main/java/com/proyecpg/hartarte/MainActivity.kt
@@ -9,13 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.proyecpg.hartarte.data.DataStoreUtil
 import com.proyecpg.hartarte.navigation.NavigationRoot
 import com.proyecpg.hartarte.ui.screens.AuthViewModel
 import com.proyecpg.hartarte.ui.screens.main.MainViewModel
-import com.proyecpg.hartarte.ui.screens.post.create.CreatePostScreen
 import com.proyecpg.hartarte.ui.theme.HartarteTheme
 import dagger.hilt.android.AndroidEntryPoint
 
