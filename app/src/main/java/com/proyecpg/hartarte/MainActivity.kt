3115f96eaba3dbae138aa3380392358cd9782217
DJosuep <josuep.garciar@gmail.com>
2023-07-04 21:50:31 -0600

    Splash Screen
    - Add SplashScreen.
    - Add new smaller logo.

--- app/src/main/java/com/proyecpg/hartarte/MainActivity.kt
+++ app/src/main/java/com/proyecpg/hartarte/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.proyecpg.hartarte.data.DataStoreUtil
@@ -27,6 +28,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         dataStoreUtil = DataStoreUtil(applicationContext)
 
