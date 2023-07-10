a1e86fba1e9c99ea3892b7843d9d983b6ea60140
DJosuep <josuep.garciar@gmail.com>
2023-07-10 14:10:08 -0600

    Add Algolia imps, Product and SearchViewModel
    - Add Algolia implementations.
    - Add Product data class.
    - Add Algolia to SearchViewModel.

--- app/src/main/java/com/proyecpg/hartarte/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/proyecpg/hartarte/ui/screens/search/SearchViewModel.kt
@@ -4,7 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.algolia.instantsearch.android.paging3.Paginator
+import com.algolia.instantsearch.android.paging3.searchbox.connectPaginator
+import com.algolia.instantsearch.compose.searchbox.SearchBoxState
+import com.algolia.instantsearch.core.connection.ConnectionHandler
+import com.algolia.instantsearch.searchbox.SearchBoxConnector
+import com.algolia.instantsearch.searchbox.connectView
+import com.algolia.instantsearch.searcher.hits.HitsSearcher
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
 import com.proyecpg.hartarte.data.post.PostRepository
+import com.proyecpg.hartarte.data.product.Product
 import com.proyecpg.hartarte.domain.model.Post
 import com.proyecpg.hartarte.utils.QueryParams
 import com.proyecpg.hartarte.utils.Resource
@@ -19,6 +30,32 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
     private val repo: PostRepository
 ): ViewModel() {
+
+    val searcher = HitsSearcher(
+        applicationID = ApplicationID("latency"),
+        apiKey = APIKey("3e3e58b5ae52a17189dda57b84dd8760"),
+        indexName = IndexName("instant_search")
+    )
+
+    //Search Box
+    val searchBoxState = SearchBoxState()
+    val searchBoxConnector = SearchBoxConnector(searcher)
+
+    //Hits
+    val hitsPaginator = Paginator(searcher){ it.deserialize(Product.serializer()) }
+
+    val connections = ConnectionHandler(searchBoxConnector)
+
+    init {
+        //Add a connection a between the data and the search box
+        connections += searchBoxConnector.connectView(searchBoxState)
+        connections += searchBoxConnector.connectPaginator(hitsPaginator)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        searcher.cancel()
+    }
 
     private val _postSearchState : MutableStateFlow<Flow<PagingData<Post>>?> = MutableStateFlow(null)
     val postSearchState get() = _postSearchState.asStateFlow()
