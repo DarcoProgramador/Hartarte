0a6bc74daaae51e175ec53c0917889c6ba2b49a4
cinthia <cinthiaduarte013@gmail.com>
2023-07-10 22:55:47 -0600

    lottie

--- app/src/main/java/com/proyecpg/hartarte/ui/screens/about/AboutScreen.kt
+++ app/src/main/java/com/proyecpg/hartarte/ui/screens/about/AboutScreen.kt
@@ -1,4 +1,24 @@
 package com.proyecpg.hartarte.ui.screens.about
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.proyecpg.hartarte.R
+
 @Composable
+fun AboutScreen()
+{
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.recipe))
+    Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        LottieAnimation(composition = composition)
+
+    }
+}
 
