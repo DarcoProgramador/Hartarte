8e0ed3165b45d466ae8750717b97a16b3701742f
DarcoProgramador <denangel123@gmail.com>
2023-07-11 13:53:11 -0600

    update PostSerial and add OnjectId

--- app/src/main/java/com/proyecpg/hartarte/data/model/PostSerial.kt
+++ app/src/main/java/com/proyecpg/hartarte/data/model/PostSerial.kt
@@ -3,6 +3,8 @@ package com.proyecpg.hartarte.data.model
 import com.algolia.instantsearch.core.highlighting.HighlightedString
 import com.algolia.instantsearch.highlighting.Highlightable
 import com.algolia.search.model.Attribute
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.indexing.Indexable
 import com.proyecpg.hartarte.utils.Constants.TITLE_FIELD
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
@@ -16,8 +18,9 @@ data class PostSerial(
     val createdAt : Long,
     val user : UserSerialize,
     val images : ArrayList<String>,
-    override val _highlightResult: JsonObject?
-) : Highlightable {
+    override val _highlightResult: JsonObject?,
+    override val objectID: ObjectID
+) : Highlightable, Indexable{
 
     val highlightedName: HighlightedString?
         get() = getHighlight(Attribute(TITLE_FIELD))
